168399bede90a0f945fe1faaa79f82ad0724e4b6
Andrew <d00133579@student.dkit.ie>
2013-03-07 18:09:37 +0000

    12 test averaging

--- src/mainapp/licenseFinder.java
+++ src/mainapp/licenseFinder.java
@@ -22,29 +22,36 @@ public class licenseFinder {
 	}
 
 	public void run() {
-		String csvFile = "";
-		// System.out.println(new File("bin/licenseplates.csv").canRead());
-		try {
-			csvFile = fileUtil.load("licenseplates.csv", "bin/").toString();
-		} catch (Exception e) {
-			System.out.println("File not found");
-		}
-		String[] format = csvFile.split(",");
-		ArrayList<String> output = new ArrayList<String>(5);
-		long then = System.nanoTime();
-		for (int i = 0; i < format.length; i++) {
-			if (isCorrectFormat(format[i])) {
-				// System.out.println(format[i]+"a");
-				if (matchesDescription(format[i])) {
-					output.add(formatInput(format[i]));
+		long average = 0;
+		for (int times = 0; times < 12; times++) {
+			String csvFile = "";
+			// System.out.println(new File("bin/licenseplates.csv").canRead());
+			try {
+				csvFile = fileUtil.load("licenseplates.csv", "bin/").toString();
+			} catch (Exception e) {
+				System.out.println("File not found");
+			}
+			String[] format = csvFile.split(",");
+			ArrayList<String> output = new ArrayList<String>(5);
+			long then = System.nanoTime();
+			for (int i = 0; i < format.length; i++) {
+				if (isCorrectFormat(format[i])) {
+					// System.out.println(format[i]+"a");
+					if (matchesDescription(format[i])) {
+						output.add(formatInput(format[i]));
+					}
 				}
 			}
+			long now = System.nanoTime();
+			average += (now - then);
 		}
-		long now = System.nanoTime();
-		System.out.println(((now - then)/Math.pow(10, 9))+" times ten to the power of minus 9 nanoSeconds");
-		for(String s: output){
-			System.out.println(s);
-		}
+		System.out.println("Average time; " + (average / 12)/Math.pow(10,9) + " seconds");
+		// System.out.println(((now - then) / Math.pow(10, 9))
+		// + " times ten to the power of minus 9 nanoSeconds");
+		// for (String s : output) {
+		// System.out.println(s);
+		// }without the for loop it just prints all the results, and the time
+		// taken
 	}
 
 	public static boolean isCorrectFormat(String input) {
@@ -60,12 +67,12 @@ public class licenseFinder {
 
 	public static boolean matchesDescription(String input) {
 		int temp = 0;
-		//System.out.println(input);
+		// System.out.println(input);
 		input = formatInput(input);
-		if (input == null){
+		if (input == null) {
 			return false;
 		}
-		//System.out.println(input);
+		// System.out.println(input);
 		// Go to the beginning, find a '0'
 		String inputTest = input.substring(0, input.indexOf(" ") - 1);
 		if (inputTest.matches(".*0.*")) {
@@ -98,57 +105,42 @@ public class licenseFinder {
 	}
 
 	private static String formatInput(String input) {
-		if(input.length() == 0){
+		if (input.length() == 0) {
 			System.out.println("Nice try, testing punk");
 			return null;
 		}
 		input = input.replaceAll("(\\s|\\t|-){1,}", "");
 		if (String.valueOf(input.charAt(3)).matches("[A-Z]")) {
-			return input.substring(0, 2)+" "+input.substring(2,4)+" "+input.substring(4);
+			return input.substring(0, 2) + " " + input.substring(2, 4) + " "
+					+ input.substring(4);
+		} else {
+			return input.substring(0, 2) + " " + input.substring(2, 3) + " "
+					+ input.substring(3);
 		}
-		else{
-			return input.substring(0, 2)+" "+input.substring(2,3)+" "+input.substring(3);
-		}
-		//From all that (\/) junk. into this (/\), which is small and all inclusive (and lazy ;))
-		/*int count = 0;
-		for (int i = 0; i < input.length(); i++) {
-			if (input.charAt(i) == ' ') {
-				count++;
-			}
-		}
-		switch (count) {
-		case 0:
-			if (String.valueOf(input.charAt(3)).matches("[A-Z]")) {
-				return input.substring(0, 2)+" "+input.substring(2,4)+" "+input.substring(4);
-			}
-			else{
-				return input.substring(0, 2)+" "+input.substring(2,3)+" "+input.substring(3);
-			}
-		case 1:
-			StringBuilder tempo = new StringBuilder(input);
-			String[] spaceFind = input.split(" ");
-			if (spaceFind[0].length() == 3) {
-				// find the values of the next two characters in [1]
-				// and find if theyre both within the ascii alphabet range
-				if (spaceFind[1].substring(0, 1).matches("[A-Z][A-Z]")) {
-					tempo.insert(spaceFind[0].length() + 2, ' ');
-				} else if (String.valueOf(spaceFind[1].charAt(0)).matches(
-						"[A-Z]")) {
-					tempo.insert(spaceFind[0].length() + 1, ' ');
-				}
-			} else {
-				tempo.insert(3, ' ');
-			}
-			System.out.println(tempo.toString() + "ye tic");
-			return tempo.toString();
-		case 2:
-			// Making assumption that the spaces are in the right place,
-			// otherwise the plate would have been dropped in isCorrectFormat()
-			return input.replaceAll("(\\s|\\t|-){1,}", " ");
-		default:
-			return null;
-			// Should never get here, but just incase
-			// someone tries to be a genius
-		}*/
+		// From all that (\/) junk. into this (/\), which is small and all
+		// inclusive (and lazy ;))
+		/*
+		 * int count = 0; for (int i = 0; i < input.length(); i++) { if
+		 * (input.charAt(i) == ' ') { count++; } } switch (count) { case 0: if
+		 * (String.valueOf(input.charAt(3)).matches("[A-Z]")) { return
+		 * input.substring(0,
+		 * 2)+" "+input.substring(2,4)+" "+input.substring(4); } else{ return
+		 * input.substring(0,
+		 * 2)+" "+input.substring(2,3)+" "+input.substring(3); } case 1:
+		 * StringBuilder tempo = new StringBuilder(input); String[] spaceFind =
+		 * input.split(" "); if (spaceFind[0].length() == 3) { // find the
+		 * values of the next two characters in [1] // and find if theyre both
+		 * within the ascii alphabet range if (spaceFind[1].substring(0,
+		 * 1).matches("[A-Z][A-Z]")) { tempo.insert(spaceFind[0].length() + 2,
+		 * ' '); } else if (String.valueOf(spaceFind[1].charAt(0)).matches(
+		 * "[A-Z]")) { tempo.insert(spaceFind[0].length() + 1, ' '); } } else {
+		 * tempo.insert(3, ' '); } System.out.println(tempo.toString() +
+		 * "ye tic"); return tempo.toString(); case 2: // Making assumption that
+		 * the spaces are in the right place, // otherwise the plate would have
+		 * been dropped in isCorrectFormat() return
+		 * input.replaceAll("(\\s|\\t|-){1,}", " "); default: return null; //
+		 * Should never get here, but just incase // someone tries to be a
+		 * genius }
+		 */
 	}
 }
